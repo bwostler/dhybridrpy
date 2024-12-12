e6610e5b0de7df10f599065704b0472ba30036b5
Migelo <miha@filetki.si>
2024-12-12 14:37:24 +0000

    populate __init__.py

---
+++ dhybridrpy/__init__.py
@@ -0,0 +1,5 @@
+from .dhybridrpy import dhybridrpy, InputFileParser
+from .containers import Timestep
+from .data import Field, Phase
+
+__all__ = ['dhybridrpy', 'InputFileParser', 'Timestep', 'Field', 'Phase']
