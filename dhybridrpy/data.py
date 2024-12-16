f0881ee9087231684779dd4a2da73665f1d3610e
Bricker Ostler <bostler@uchicago.edu>
2024-12-16 11:38:17 -0600

    Removed commented out old Data class.

--- dhybridrpy/data.py
+++ dhybridrpy/data.py
@@ -5,62 +5,6 @@ import matplotlib.pyplot as plt
 from typing import Union
 from matplotlib.axes import Axes
 
-# class Data:
-#     def __init__(self, file_path: str, name: str, timestep: int):
-#         self.file_path = file_path
-#         self.name = name
-#         self.timestep = timestep
-#         self._data_dict = {}
-
-#     def _get_hdf5_data(self) -> dict:
-#         with h5py.File(self.file_path, "r") as file:
-#             data = file["DATA"][:].T
-#             x1lims = file["AXIS"]["X1 AXIS"][:]
-#             x2lims = file["AXIS"]["X2 AXIS"][:]
-#             N1, N2 = data.shape
-
-#             dx1 = (x1lims[1] - x1lims[0]) / N1
-#             dx2 = (x2lims[1] - x2lims[0]) / N2
-#             x_coords = dx1 * np.arange(N1) + dx1 / 2 + x1lims[0]
-#             y_coords = dx2 * np.arange(N2) + dx2 / 2 + x2lims[0]
-
-#             return {
-#                 self.name: data,
-#                 f"{self.name}_x": x_coords,
-#                 f"{self.name}_y": y_coords,
-#                 f"{self.name}_xlim": x1lims,
-#                 f"{self.name}_ylim": x2lims
-#             }
-
-#     @property
-#     def data_dict(self) -> dict:
-#         if not self._data_dict:
-#             self._data_dict = self._get_hdf5_data()
-#         return self._data_dict
-
-#     def _get_property(self, prop: str) -> np.array:
-#         return self.data_dict[f"{self.name}{prop}"]
-
-#     @property
-#     def data(self) -> np.array:
-#         return self._get_property("")
-
-#     @property
-#     def xdata(self) -> np.array:
-#         return self._get_property("_x")
-
-#     @property
-#     def ydata(self) -> np.array:
-#         return self._get_property("_y")
-
-#     @property
-#     def xlimdata(self) -> np.array:
-#         return self._get_property("_xlim")
-
-#     @property
-#     def ylimdata(self) -> np.array:
-#         return self._get_property("_ylim")
-
 class Data:
     def __init__(self, file_path: str, name: str, timestep: int):
         self.file_path = file_path
