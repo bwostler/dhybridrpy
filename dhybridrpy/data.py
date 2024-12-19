9a977df9a7a2cfcc6720a74950d2d603fae4f25b
Bricker Ostler <bostler@uchicago.edu>
2024-12-19 15:08:14 -0600

    Implemented lazy evaluation of Data class functions with dask

--- dhybridrpy/data.py
+++ dhybridrpy/data.py
@@ -1,32 +1,48 @@
 import h5py
 import numpy as np
+import dask.array as da
 import matplotlib.pyplot as plt
-import copy
 
 from matplotlib.axes import Axes
+from matplotlib.collections import QuadMesh
+from typing import Tuple
+from dask import delayed
 
 class Data:
-    def __init__(self, file_path: str, name: str, timestep: int):
+    def __init__(self, file_path: str, name: str, timestep: int, lazy_evaluate: bool):
         self.file_path = file_path
         self.name = name
         self.timestep = timestep
+        self.lazy_evaluate = lazy_evaluate
         self._data_dict = {}
         self._data_shape = None
 
-    def _get_coordinate_limits(self, axis_name: str) -> np.array:
+    def _get_coordinate_limits(self, axis_name: str) -> np.ndarray | da.Array:
         """Retrieve a specific axis from the file."""
         if axis_name not in self._data_dict:
-            with h5py.File(self.file_path, "r") as file:
-                self._data_dict[axis_name] = file["AXIS"][axis_name][:]
+
+            def coordinate_limits_helper() -> np.ndarray:
+                with h5py.File(self.file_path, "r") as file:
+                    return file["AXIS"][axis_name][:]
+
+            if self.lazy_evaluate:
+                self._data_dict[axis_name] = delayed(coordinate_limits_helper)()
+            else:
+                self._data_dict[axis_name] = coordinate_limits_helper()
+
         return self._data_dict[axis_name]
 
-    def _compute_coordinates(self, axis_name: str, size: int) -> np.array:
+    def _compute_coordinates(self, axis_name: str, size: int) -> np.ndarray | da.Array:
         """Compute coordinates for a given axis."""
         key = f"{axis_name} coords"
         if key not in self._data_dict:
             axis_limits = self._get_coordinate_limits(axis_name)
             delta = (axis_limits[1] - axis_limits[0]) / size
-            self._data_dict[key] = delta*np.arange(size) + (delta/2) + axis_limits[0]
+            if self.lazy_evaluate:
+                grid = da.arange(size, chunks="auto")
+            else:
+                grid = np.arange(size)
+            self._data_dict[key] = delta*grid + (delta/2) + axis_limits[0]
         return self._data_dict[key]
     
     def _get_data_shape(self) -> tuple:
@@ -37,30 +53,39 @@ class Data:
         return self._data_shape
 
     @property
-    def data(self) -> np.array:
+    def data(self) -> np.ndarray | da.Array:
         """Retrieve the data values."""
         if self.name not in self._data_dict:
-            with h5py.File(self.file_path, "r") as file:
-                self._data_dict[self.name] = file["DATA"][:].T
+
+            def data_helper() -> np.ndarray:
+                """Load the data from the file."""
+                with h5py.File(self.file_path, "r") as file:
+                    return file["DATA"][:].T
+
+            if self.lazy_evaluate:
+                self._data_dict[self.name] = delayed(data_helper)()
+            else:
+                self._data_dict[self.name] = data_helper()
         return self._data_dict[self.name]
+   
 
     @property
-    def xdata(self) -> np.array:
+    def xdata(self) -> np.ndarray | da.Array:
         """Retrieve x-coordinates."""
         return self._compute_coordinates("X1 AXIS", self._get_data_shape()[0])
 
     @property
-    def ydata(self) -> np.array:
+    def ydata(self) -> np.ndarray | da.Array:
         """Retrieve y-coordinates."""
         return self._compute_coordinates("X2 AXIS", self._get_data_shape()[1])
 
     @property
-    def xlimdata(self) -> np.array:
+    def xlimdata(self) -> np.ndarray | da.Array:
         """Retrieve x-axis limits."""
         return self._get_coordinate_limits("X1 AXIS")
 
     @property
-    def ylimdata(self) -> np.array:
+    def ylimdata(self) -> np.ndarray | da.Array:
         """Retrieve y-axis limits."""
         return self._get_coordinate_limits("X2 AXIS")
 
@@ -75,17 +100,17 @@ class Data:
         colormap: str = "viridis",
         show_colorbar: bool = True,
         colorbar_label: str | None = None,
-        save: bool = False,
-        save_name: str | None = None,
-        save_format: str = "jpg",
-        show: bool = True,
+        # save: bool = False,
+        # save_name: str | None = None,
+        # save_format: str = "jpg",
+        # show: bool = True,
         **kwargs
-    ) -> Axes:
+    ) -> Tuple[Axes, QuadMesh]:
 
         if ax is None:
             fig, ax = plt.subplots(figsize=(8, 6), dpi=dpi)
 
-        X, Y = np.meshgrid(self.xdata, self.ydata, indexing='ij')
+        X, Y = np.meshgrid(self.xdata, self.ydata, indexing="ij")
         mesh = ax.pcolormesh(
             X, Y, self.data, cmap=colormap, shading="auto", **kwargs
         )
@@ -98,15 +123,15 @@ class Data:
             cbar = plt.colorbar(mesh, ax=ax)
             cbar.set_label(colorbar_label if colorbar_label else f"{self.name}")
 
-        if save:
-            if not save_name:
-                save_name = f"{self.name}_timestep{self.timestep}"
-            plt.savefig(f"{save_name}.{save_format}", dpi=dpi)
+        # if save:
+        #     if not save_name:
+        #         save_name = f"{self.name}_timestep{self.timestep}"
+        #     plt.savefig(f"{save_name}.{save_format}", dpi=dpi)
 
-        if show:
-            plt.show()
+        # if show:
+        #     plt.show()
 
-        return ax
+        return ax, mesh
 
     def __repr__(self) -> str:
         attrs = ", ".join(
@@ -116,12 +141,12 @@ class Data:
 
 
 class Field(Data):
-    def __init__(self, file_path: str, name: str, timestep: int, origin: str):
-        super().__init__(file_path, name, timestep)
+    def __init__(self, file_path: str, name: str, timestep: int, lazy_evaluate: bool, origin: str):
+        super().__init__(file_path, name, timestep, lazy_evaluate)
         self.origin = origin # e.g., "External"
 
 
 class Phase(Data):
-    def __init__(self, file_path: str, name: str, timestep: int, species: int | str):
-        super().__init__(file_path, name, timestep)
+    def __init__(self, file_path: str, name: str, timestep: int, lazy_evaluate: bool, species: int | str):
+        super().__init__(file_path, name, timestep, lazy_evaluate)
         self.species = species
