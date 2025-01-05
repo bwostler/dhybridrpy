8d2012edae4e969b456466a64bcfada95df6ea37
Bricker Ostler <bostler@uchicago.edu>
2025-01-04 18:30:15 -0600

    Removed redundant child classes of Container

--- dhybridrpy/containers.py
+++ dhybridrpy/containers.py
@@ -3,12 +3,12 @@ from typing import Callable
 from .data import Field, Phase, Raw
 
 class Container:
-    def __init__(self, data_dict: dict, timestep: int, container_type: str, key_name: str, default_key_value: int | str):
+    def __init__(self, data_dict: dict, timestep: int, container_type: str, kwarg: str, default_kwarg_value: int | str):
         self.data_dict = data_dict
         self.timestep = timestep
         self.container_type = container_type.capitalize() if not container_type.isupper() else container_type
-        self.key_name = key_name
-        self.default_key_value = default_key_value
+        self.kwarg = kwarg
+        self.default_kwarg_value = default_kwarg_value
 
     def __getattr__(self, data_name: str) -> Callable:
         def get_data(*args, **kwargs) -> Field | Phase | Raw:
@@ -17,79 +17,64 @@ class Container:
             if len(args) + len(kwargs) > 1:
                 raise TypeError(f"Expected at most one argument.")
 
-            # If the argument is a key value pair, make sure the key is the expected key_name
-            if kwargs and self.key_name not in kwargs:
-                raise TypeError(f"Argument name must be '{self.key_name}'.")
+            # If the argument is a key value pair, make sure the key is the expected kwarg
+            if kwargs and self.kwarg not in kwargs:
+                raise TypeError(f"Argument name must be '{self.kwarg}'.")
 
             # Grab the value if no key is used, otherwise grab the key's value. If kwargs is empty, return the default value.
-            key = args[0] if args else kwargs.get(self.key_name, self.default_key_value)
+            data_key = args[0] if args else kwargs.get(self.kwarg, self.default_kwarg_value)
 
-            # If key is a string, make sure it's capitalized
-            if isinstance(key, str) and not key.isupper():
-                key = key.capitalize()
+            # If data_key is a string, make sure it's capitalized
+            if isinstance(data_key, str) and not data_key.isupper():
+                data_key = data_key.capitalize()
 
-            # Check if key, data_name (e.g. "Total", "Bx") exist in data_dict at this timestep
-            if key not in self.data_dict:
-                raise AttributeError(f"{self.container_type} with {self.key_name} '{key}' not found at timestep {self.timestep}.")
-            if data_name not in self.data_dict[key]:
-                raise AttributeError(f"{self.container_type} '{data_name}' with {self.key_name} '{key}' not found at timestep {self.timestep}.")
+            # Check if data_key, data_name (e.g. "Total", "Bx") exist in data_dict at this timestep
+            if data_key not in self.data_dict:
+                raise AttributeError(f"{self.container_type} with {self.kwarg} '{data_key}' not found at timestep {self.timestep}.")
+            if data_name not in self.data_dict[data_key]:
+                raise AttributeError(f"{self.container_type} '{data_name}' with {self.kwarg} '{data_key}' not found at timestep {self.timestep}.")
 
-            return self.data_dict[key][data_name]
+            return self.data_dict[data_key][data_name]
 
         return get_data
 
     def __repr__(self) -> str:
         data_summary = {
-            f"{self.key_name} = {key}": sorted(data.keys()) for key, data in self.data_dict.items()
+            f"{self.kwarg} = {key}": sorted(value.keys()) for key, value in self.data_dict.items()
         }
         return f"{self.container_type}s at timestep {self.timestep} = {data_summary}"
-
-
-class FieldContainer(Container):
-    def __init__(self, fields_dict: dict, timestep: int):
-        super().__init__(
-            data_dict=fields_dict,
-            timestep=timestep, 
-            container_type="Field", 
-            key_name="origin", 
-            default_key_value="Total"
-        )
-
-
-class PhaseContainer(Container):
-    def __init__(self, phases_dict: dict, timestep: int):
-        super().__init__(
-            data_dict=phases_dict, 
-            timestep=timestep, 
-            container_type="Phase", 
-            key_name="species", 
-            default_key_value=1
-        )
-
-
-class RawContainer(Container):
-    def __init__(self, raw_dict: dict, timestep: int):
-        super().__init__(
-            data_dict=raw_dict, 
-            timestep=timestep, 
-            container_type="Raw file", 
-            key_name="species", 
-            default_key_value=1
-        )
 
 
 class Timestep:
     def __init__(self, timestep: int):
         self.timestep = timestep
         self._fields_dict = {"Total": {}, "External": {}, "Self": {}}
-        self._phases_dict = defaultdict(lambda: {})
-        self._raw_dict = defaultdict(lambda: {})
+        self._phases_dict = defaultdict(dict)
+        self._raw_dict = defaultdict(dict)
 
-        # User uses these attributes to dynamically resolve a given field or phase using FieldContainer
-        # PhaseContainer, or RawContainer __getattr__ dunder function.
-        self.fields = FieldContainer(self._fields_dict, timestep)
-        self.phases = PhaseContainer(self._phases_dict, timestep)
-        self.raw_files = RawContainer(self._raw_dict, timestep)
+        # User uses these attributes to dynamically resolve a given field, phase, 
+        # or raw file using Container __getattr__ dunder function.
+        self.fields = Container(
+            data_dict=self._fields_dict, 
+            timestep=timestep, 
+            container_type="Field", 
+            kwarg="origin", 
+            default_kwarg_value="Total"
+        )
+        self.phases = Container(
+            data_dict=self._phases_dict, 
+            timestep=timestep, 
+            container_type="Phase", 
+            kwarg="species", 
+            default_kwarg_value=1
+        )
+        self.raw_files = Container(
+            data_dict=self._raw_dict, 
+            timestep=timestep, 
+            container_type="Raw file", 
+            kwarg="species", 
+            default_kwarg_value=1
+        )
 
     def add_field(self, field: Field) -> None:
         if field.origin not in self._fields_dict:
