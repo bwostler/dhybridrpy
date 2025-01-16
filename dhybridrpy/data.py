8398e44fb5a5137d1fbb4700105d699c474b45f5
Bricker Ostler <bostler@uchicago.edu>
2025-01-16 10:28:04 -0600

    Simplified title update for child classes of Data

--- dhybridrpy/data.py
+++ dhybridrpy/data.py
@@ -210,24 +210,14 @@ class Field(Data):
     def __init__(self, file_path: str, name: str, timestep: int, lazy: bool, field_type: str):
         super().__init__(file_path, name, timestep, lazy)
         self.type = field_type # The type of field, e.g., "External"
-
-    def plot(self, **kwargs) -> Tuple[Axes, Union[Line2D, QuadMesh, PathCollection]]:
-        type_info = f" (type = {self.type})"
-        if not self._plot_title.endswith(type_info):
-            self._plot_title += type_info
-        return super().plot(**kwargs)
+        self._plot_title += f" (type = {self.type})"
 
 
 class Phase(Data):
     def __init__(self, file_path: str, name: str, timestep: int, lazy: bool, species: Union[int, str]):
         super().__init__(file_path, name, timestep, lazy)
         self.species = species
-
-    def plot(self, **kwargs) -> Tuple[Axes, Union[Line2D, QuadMesh, PathCollection]]:
-        species_info = f" (species = {self.species})"
-        if not self._plot_title.endswith(species_info):
-            self._plot_title += species_info
-        return super().plot(**kwargs)
+        self._plot_title += f" (species = {self.species})"
 
 
 class Raw(BaseProperties):
